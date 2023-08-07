57abc989897624f47374337c73d32a4a8081c803
imamulahsan <imamulahsan@hotmail.com>
2023-08-07 23:52:19 +0200

    added mvc

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/errorMiddleware");
+
+//routes path
+const authRoutes = require("./routes/authRoutes");
+
 
 //dotenv
 dotenv.config();
@@ -20,9 +25,13 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 8080;
+
+//API routes
+app.use("/api/v1/auth", authRoutes);
 
 //listen server
 app.listen(8080, () =>{
